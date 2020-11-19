d81d0e74bd0a15460d342d26e0f7d19cfc9e8683
atul kaushal <atul.hkaushal@gmail.com>
2020-11-19 14:25:38 -0600

    Added configuration for docker compose.

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ call docker build -t akaushal85/spring-boot-docker .
 call docker push akaushal85/spring-boot-docker:latest
 call docker network rm my-network
 call docker network create my-network
-call docker run -d --name mysql-docker --network=my-network  -e MYSQL_ROOT_PASSWORD=root mysql
+call docker run -d --name mysql-docker --network=my-network -e MYSQL_ROOT_PASSWORD=root mysql
 call docker run -d -p 8080:8080 --network=my-network akaushal85/spring-boot-docker
